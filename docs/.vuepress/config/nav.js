2068441b671542ab2a5b2fee51f9f6baa471253d
zhangyaping <916459426@qq.com>
2020-10-25 22:27:34 +0800

    commit

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,7 +1,7 @@
 module.exports = [
   {text: '首页', link: '/'},
   {
-    text: '技术',
+    text: '文章',
     link: '/technology/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
     items: [
       {text: '技术文档', link: '/pages/9a7ee40fc232253e/'}, // 这些link是在相应md文件定义的永久链接。另外，注意结尾是有斜杠的
@@ -11,16 +11,6 @@ module.exports = [
         {text: '《JavaScript高级程序设计》笔记', link: '/note/js/'},
         {text: '《ES6 教程》笔记', link: '/note/es6/'},
       ]}
-    ]
-  },
-  {
-    text: '更多', 
-    link: '/more/',
-    items: [
-      {text: '学习', link: '/pages/f2a556/'},
-      {text: '面试', link: '/pages/aea6571b7a8bae86/'},
-      {text: '心情杂货', link: '/pages/2d615df9a36a98ed/'},
-      {text: '友情链接', link: '/friends/'},
     ]
   },
   {text: '关于', link: '/about/'},
